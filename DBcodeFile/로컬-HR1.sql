ffed2ae1cceba20338f6578cfe23d03979174300
daehyok <kilcs12@hanmail.net>
2021-04-15 18:00:52 +0900

    Add DBcodeFile dir

---
+++ DBcodeFile/로컬-HR1.sql
@@ -0,0 +1,87 @@
+-- 주석
+
+DROP TABLE userTBL;
+DROP TABLE buyTBL;
+
+CREATE TABLE userTBL(
+    userID CHAR(8) NOT NULL PRIMARY KEY, --사용자 id PK
+    userNAME NVARCHAR2(10) NOT NULL,-- 이름
+    birthYEAR NUMBER(4) NOT NULL, -- 출생년도
+    addr NCHAR(2) NOT NULL, --지역
+    mobile CHAR(3), --휴대폰 국번
+    mobile2 CHAR(8), --나머지 전화번호 하이픈(-)제외
+    height NUMBER(3), --키
+    mDate DATE -- 회원 가입일
+);
+
+CREATE TABLE buyTBL(
+    idNum NUMBER(8) NOT NULL PRIMARY KEY, --순번(PK)
+    userID CHAR(8) NOT NULL, --아이디(FK)
+    prodName NCHAR(6) NOT NULL, --물품명
+    groupName NCHAR(4), --분류
+    price NUMBER(8) NOT NULL, --단가
+    amount NUMBER(3) NOT NULL, --수량
+    
+    FOREIGN KEY (userID) REFERENCES userTBL(userID)
+);
+
+CREATE SEQUENCE idSEQ;
+
+DROP SEQUENCE isSEQ;
+INSERT INTO userTBL VALUES('LSG','이승기',1987,'서울','011','11111111',182,'2008-8-8');
+INSERT INTO userTBL VALUES('KBS','김범수',1979,'경남','011','22222222',173,'2012-4-4');
+INSERT INTO userTBL VALUES('KKH','김경호',1971,'전남','019','3333333',177,'2007-7-7');
+INSERT INTO userTBL VALUES('JYP','조용필',1950,'경기',011,'44444444',166,'2009-4-4');
+INSERT INTO userTBL VALUES('SSK','성시경',1979,'서울',NULL,NULL,186,'2013-12-12');
+INSERT INTO userTBL VALUES('LJB','임재범',1963,'서울','016','55555555',182,'2009-9-9');
+INSERT INTO userTBL VALUES('YJS','윤종신',1969,'경남',NULL,NULL,170,'2005-5-5');
+INSERT INTO userTBL VALUES('EJW','은지원',1972,'경북','011','66666666',174,'2014-3-3');
+INSERT INTO userTBL VALUES('JKW','조관우',1965,'경기','018','77777777',172,'2010-10-10');
+INSERT INTO userTBL VALUES('BBK','바비킴',1973,'서울','010','88888888',176,'2013-5-5');
+
+
+
+INSERT INTO buyTBL VALUES(idSEQ.NEXTVAL,'KBS','운동화',NULL,30,2);
+INSERT INTO buyTBL VALUES(idSEQ.NEXTVAL,'KBS','노트북','전자', 1000,1);
+INSERT INTO buyTBL VALUES(idSEQ.NEXTVAL,'JYP','모니터', '전자', 200,1);
+INSERT INTO buyTBL VALUES(idSEQ.NEXTVAL,'BBK','모니터', '전자', 200,5);
+INSERT INTO buyTBL VALUES(idSEQ.NEXTVAL,'KBS','청바지', '의류', 50,3);
+INSERT INTO buyTBL VALUES(idSEQ.NEXTVAL,'BBK','메모리', '전자', 80,10);
+INSERT INTO buyTBL VALUES(idSEQ.NEXTVAL,'SSK','책', '서적', 15,5);
+INSERT INTO buyTBL VALUES(idSEQ.NEXTVAL,'EJW','책', '서적', 15,2);
+INSERT INTO buyTBL VALUES(idSEQ.NEXTVAL,'EJW','청바지', '의류', 50,1);
+INSERT INTO buyTBL VALUES(idSEQ.NEXTVAL,'BBK','운동화', NULL, 30,2);
+INSERT INTO buyTBL VALUES(idSEQ.NEXTVAL,'EJW','책', '서적', 15,1);
+INSERT INTO buyTBL VALUES(idSEQ.NEXTVAL,'BBK','운동화', NULL, 30,2);
+
+
+SELECT * FROM userTBL;
+SELECT * FROM buyTBL;
+
+-- 관계 연산자 사용 
+SELECT userID,userName
+FROM USERTBL
+WHERE BIRTHYEAR >= 1970 AND HEIGHT >= 182;
+
+--BETWEEN ... AND
+SELECT userName
+FROM USERTBL
+WHERE HEIGHT BETWEEN 180 AND 183;
+
+--IN()
+SELECT userName, addr
+FROM USERTBL
+WHERE addr IN ('경남','전남','경북');
+
+--LIKE  *** '%'== 모든 문자를 뜻함 , '_' == 문자 한개를 의미함 , 
+SELECT userName,height
+FROM userTBL
+WHERE userName LIKE '김%';
+
+--ORDER BY
+--ASC 오름차순 정렬
+SELECT userName, mDate FROM userTBL ORDER BY mDate ASC;
+--DESC 내립차순 정렬
+SELECT userName, mDate FROM userTBL ORDER BY mDate ASC;
+-- 
+
